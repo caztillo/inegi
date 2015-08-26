21fa089773388cbb2ca3eefe9e606a78ce235f1f
Pablo Castillo <caztillo@gmail.com>
2015-08-25 21:18:54 -0500

    terminado

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', array('uses' => 'SoapController@index'));
 
 Route::get('/soap', array('uses' => 'SoapController@index'));
 Route::post('/soap-webservice', array('uses' => 'SoapController@webservice'));
